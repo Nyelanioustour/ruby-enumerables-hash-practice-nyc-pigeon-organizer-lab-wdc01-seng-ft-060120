41145082f378fcbcb4b2fd29cc6fe057161a2c4b
Shawn Alexander <shawn.k.alexander@gmail.com>
2020-05-25 19:07:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,12 +3,12 @@ def nyc_pigeon_organizer(data)
   data.each_with_obect({}) do |key, element, new_hash|
     element.each do |inner_key, value|
       value.each do |value|
-        binding.pry 
+        
   end 
  end 
  end 
       
- 
+ binding.pry 
  
  
 end
