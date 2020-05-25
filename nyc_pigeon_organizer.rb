fb2d2e12d9287d8751988eb7a4fa2faded627ae1
Shawn Alexander <shawn.k.alexander@gmail.com>
2020-05-25 19:12:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,13 +2,19 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   data.each_with_obect({}) do |key, element, new_hash|
     element.each do |inner_key, value|
-      value.each do |value|
-        
+      value.each do |name|
+        if !new_hash[name]
+          new_hash[name] - {}
+        end
+        if !new_hash[name][key]
+          new_hash[name][key] = []
+        end
+        new_hash[name][key].push(inner_key.to_s)
   end 
  end 
  end 
       
- binding.pry 
+ 
  
  
 end
